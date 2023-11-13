cd808e74b04d7a550dfaccd81a36a0733c73d5a7
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2023-11-13 10:56:41 +0200

    Examples

---
+++ src/com/softserve/edu08/wrapper/A.java
@@ -0,0 +1,14 @@
+package com.softserve.edu08.wrapper;
+
+public class A { //implements Serializable {
+    
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String toString() {
+		return "A [getClass()=" + getClass()
+			+ ", hashCode()=" + hashCode()
+			+ ", toString()" + "]";
+	}
+
+}
