afcb140aab952d2cf463caf1c4c0bb703c0b5cb2
obutr <butrym@karazin.ua>
2023-11-16 21:03:50 +0200

    Add solution to first practical task

--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -1,12 +1,20 @@
 package com.softserve.edu01.pt;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
         // Practical tasks
 
         // Task 1
-
+        var scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+        String s3 = scanner.nextLine();
+        System.out.println(s3);
+        System.out.println(s2);
+        System.out.println(s1);
         // Task 2
 
         // Task 3...
