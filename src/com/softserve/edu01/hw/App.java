36c1c60df3868ec95387bb6ee10966834acf71cf
Artur Ivanov <iarturv19@gmail.com>
2023-11-19 23:53:36 +0200

    Solution to first homework

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -11,8 +11,38 @@ public class App {
 
         // Task 1
         App.task1();
+        var scanner = new Scanner(System.in);
+        System.out.print("Enter the radius: ");
+        int inputInt = scanner.nextInt();
+        System.out.println("Perimeter = " + (2 * 3.14 * inputInt));
+        System.out.print("Circle = " + Math.pow((3.14 * inputInt), 2));
 
-        // Task 2...
+        // Task 2
+        var scanner = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String intName = scanner.nextLine();
+        System.out.println("Hi, " + intName + "!");
+        System.out.print("Where do you live? ");
+        String intCity = scanner.nextLine();
+        System.out.print(intCity + " is a beautiful city!");
+
+        // Task 3
+        var scanner = new Scanner(System.in);
+        System.out.print("Enter the cost per minute 1: ");
+        int inputCost1 = scanner.nextInt();
+        System.out.print("Enter the cost per minute 2: ");
+        int inputCost2 = scanner.nextInt();
+        System.out.print("Enter the cost per minute 3: ");
+        int inputCost3 = scanner.nextInt();
+        System.out.print("Enter the call duration 1: ");
+        int inputDuration1 = scanner.nextInt();
+        System.out.print("Enter the call duration 2: ");
+        int inputDuration2 = scanner.nextInt();
+        System.out.print("Enter the call duration 3: ");
+        int inputDuration3 = scanner.nextInt();
+        System.out.println("Price for the first call = " + (inputCost1 * inputDuration1));
+        System.out.println("Price for the second call = " + (inputCost2 * inputDuration2));
+        System.out.print("Price for the third call = " + (inputCost3 * inputDuration3));
     }
 
     public static void task1 () {
