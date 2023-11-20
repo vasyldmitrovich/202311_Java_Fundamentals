460577269108d592d20ba507ac3f3a0ac55de18d
obutr <butrym@karazin.ua>
2023-11-20 19:18:31 +0200

    Clean the code, fix nextLine bug

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -1,37 +1,28 @@
 package com.softserve.edu01.hw;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class App {
-
-
-
     public static void main(String[] args) {
         // Homeworks
 
         // Task 1
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.ROOT);
         System.out.println("input radius");
         double radius = scanner.nextDouble();
+        scanner.nextLine();
         double area = Math.PI * radius * radius;
-        double perimetr = Math.PI * 2 * radius;
-        System.out.println("area is" + area);
-        System.out.println("perimetr is" + perimetr);
-
-
-
+        double perimeter = Math.PI * 2 * radius;
+        System.out.println("area is " + area);
+        System.out.println("perimeter is " + perimeter);
 
         // Task 2...
-
-
         System.out.println("What is your name: ");
         String answer = scanner.nextLine();
-        System.out.println("You name is" + answer);
+        System.out.println("You name is " + answer);
         System.out.println("Where do you live: ");
         String answer1 = scanner.nextLine();
-        System.out.println("You live in" + answer1);
+        System.out.println("You live in " + answer1);
     }
-
-
-    }
-
+}
