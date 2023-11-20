a32168e6c0cae80ee24559eea27bff0efcf52028
adrdvz42 <151073462+adrdvz42@users.noreply.github.com>
2023-11-20 18:44:28 +0200

    First homework

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -4,21 +4,34 @@ import java.util.Scanner;
 
 public class App {
 
-    public static Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
         // Homeworks
 
         // Task 1
-        App.task1();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input radius");
+        double radius = scanner.nextDouble();
+        double area = Math.PI * radius * radius;
+        double perimetr = Math.PI * 2 * radius;
+        System.out.println("area is" + area);
+        System.out.println("perimetr is" + perimetr);
+
+
+
 
         // Task 2...
+
+
+        System.out.println("What is your name: ");
+        String answer = scanner.nextLine();
+        System.out.println("You name is" + answer);
+        System.out.println("Where do you live: ");
+        String answer1 = scanner.nextLine();
+        System.out.println("You live in" + answer1);
     }
 
-    public static void task1 () {
-        System.out.println("Input your name");
-        String name = scanner.nextLine();
 
-        System.out.println("Your name is: " + name);
     }
-}
+
--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -1,5 +1,7 @@
 package com.softserve.edu01.pt;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
@@ -7,9 +9,40 @@ public class App {
 
         // Task 1
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input answer1: ");
+        String an1 = scanner.nextLine();
+        System.out.println("Input answer2: ");
+        String an2 = scanner.nextLine();
+        System.out.println("Input answer3: ");
+        String an3 = scanner.nextLine();
+        System.out.println(an3);
+        System.out.println(an2);
+        System.out.println(an1);
+
+
         // Task 2
 
+        System.out.println("Input first number: ");
+        int answer1 = scanner.nextInt();
+        System.out.println("Input second number: ");
+        int answer2 = scanner.nextInt();
+        System.out.println("Input third number: ");
+        int answer3 = scanner.nextInt();
+        double average = (answer1 + answer2 + answer3) / 3;
+        System.out.println("average number" + average);
+
         // Task 3...
+
+
+        System.out.println("Input a: ");
+        double a = scanner.nextDouble();
+        System.out.println("Input b: ");
+        double b = scanner.nextDouble();
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
 
     }
 }
