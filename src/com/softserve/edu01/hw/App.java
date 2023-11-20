956b743e1931facd12771193b8e203bbc934e947
Volodymyr.Dinul <vdinul@yahoo.com>
2023-11-20 19:46:57 +0200

    VDinul. Practical tasks for EDU1

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -12,13 +12,37 @@ public class App {
         // Task 1
         App.task1();
 
-        // Task 2...
+        // Task 2
+        App.task2();
     }
 
     public static void task1 () {
-        System.out.println("Input your name");
-        String name = scanner.nextLine();
+        System.out.println("Enter string one:  ");
+        String name1 = scanner.nextLine();
+        System.out.println("Enter string two:  ");
+        String name2 = scanner.nextLine();
+        System.out.println("Enter string three:  ");
+        String name3 = scanner.nextLine();
 
-        System.out.println("Your name is: " + name);
+        System.out.println(name3);
+        System.out.println(name2);
+        System.out.println(name1);
+    }
+
+    public static void task2() {
+        System.out.println("Enter the first number:  ");
+        double firstNum = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Enter the second number:  ");
+        double secondNum = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Enter the third number:  ");
+        double thirdNum = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println( ( firstNum + secondNum + thirdNum ) / 3 );
+
     }
 }
