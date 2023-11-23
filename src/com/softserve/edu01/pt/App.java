d914dd2aa129f67213cc7a0864ace13c6a07a6bd
Olksandr Kornilenko <48486842+kornilenko-o@users.noreply.github.com>
2023-11-23 19:36:18 +0200

    edy01 pt2 bug fix

--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -25,9 +25,9 @@ public class App {
         System.out.println("Task 2");
         System.out.print("Input first number: ");
         double number1 = Double.parseDouble(scanner.nextLine());
-        System.out.print("Input first number: ");
+        System.out.print("Input second number: ");
         double number2 = Double.parseDouble(scanner.nextLine());
-        System.out.print("Input first number: ");
+        System.out.print("Input third number: ");
         double number3 = Double.parseDouble(scanner.nextLine());
         System.out.println("Average is: " + ((number1+number2+number3)/3));
 
