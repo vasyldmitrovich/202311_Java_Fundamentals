ed8277c1ccad9d726b2a8999830bc290df36aeb8
Olksandr Kornilenko <48486842+kornilenko-o@users.noreply.github.com>
2023-11-23 18:26:53 +0200

    edy01 add solution hw3

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -8,7 +8,6 @@ public class App {
 
     public static void main(String[] args) {
         // Homeworks
-
         // Task 1
         App.task1();
         // Task 2
@@ -35,7 +34,27 @@ public class App {
     }
 
     public static void task3(){
-
+        System.out.println("Task 3");
+        System.out.print("Enter first call cost: ");
+        double c1 = Double.parseDouble(scanner.nextLine());
+        System.out.print("Enter first call duration (min): ");
+        int t1 = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter first call cost: ");
+        double c2 = Double.parseDouble(scanner.nextLine());
+        System.out.print("Enter first call duration (min): ");
+        int t2 = Integer.parseInt(scanner.nextLine());
+        System.out.print("Enter first call cost: ");
+        double c3 = Double.parseDouble(scanner.nextLine());
+        System.out.print("Enter first call duration (min): ");
+        int t3 = Integer.parseInt(scanner.nextLine());
+        double firstCallCost = c1 * t1;
+        double secondCallCost = c2 * t2;
+        double thirdCallCost = c3 * t3;
+        double totalCost = firstCallCost + secondCallCost + thirdCallCost;
+        System.out.println("First call cost: " + firstCallCost);
+        System.out.println("Second call cost: " + secondCallCost);
+        System.out.println("Third call cost: " + thirdCallCost);
+        System.out.println("Total cost: " + totalCost);
     }
 
 
