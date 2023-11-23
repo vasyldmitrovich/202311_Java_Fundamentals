b6d9a6e5efc76191a0880068968cff4c85c4a8b2
Olksandr Kornilenko <48486842+kornilenko-o@users.noreply.github.com>
2023-11-23 18:16:51 +0200

    edy01 add solution hw1

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -11,14 +11,27 @@ public class App {
 
         // Task 1
         App.task1();
-
-        // Task 2...
+        // Task 2
+        task2();
+        //Task 3
+        task3();
     }
 
-    public static void task1 () {
-        System.out.println("Input your name");
-        String name = scanner.nextLine();
-
-        System.out.println("Your name is: " + name);
+    public static void task1(){
+        System.out.println("Task 1");
+        System.out.print("Enter radius: ");
+        double radius = Double.parseDouble(scanner.nextLine());
+        System.out.println("Perimeter is " + (2 * radius * Math.PI));
+        System.out.println("Area is " + (Math.PI * Math.pow(radius, 2)));
     }
+
+    public static void task2 (){
+
+    }
+
+    public static void task3(){
+
+    }
+
+
 }
