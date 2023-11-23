d4db6a699f755d71f5ef726257e1c2a7be138269
Olena Ponomarenko <ponomarenkoalena555@gmail.com>
2023-11-23 17:43:00 +0200

    Homework tasks 1,2 lesson 3

--- src/com/softserve/edu03/hw/App.java
+++ src/com/softserve/edu03/hw/App.java
@@ -13,20 +13,47 @@ public class App {
 
         // Task 2...
         App.task2();
+
+        // Task 3...
+        // App.task3();
     }
 
     public static void task1 () {
-        System.out.println("Input your name");
-        String name = scanner.nextLine();
+        System.out.println("Enter the Side 1: ");
+        double s1 = Double.parseDouble(scanner.nextLine());
+        System.out.println("Enter the Side 2: ");
+        double s2 = Double.parseDouble(scanner.nextLine());
+        System.out.println("Enter the Side 3: ");
+        double s3 = Double.parseDouble(scanner.nextLine());
 
-        System.out.println("Your name is: " + name);
+        System.out.println("Area of the triangle: " + areaTriangle(s1, s2, s3));
+
+    }
+
+    public static double areaTriangle(double s1, double s2, double s3){
+           double p = (s1 + s2 + s3)/2;
+           double area = Math.pow(p*(p-s1)*(p-s2)*(p-s3), 1.0/2.0);
+           return area;
     }
 
     public static void task2 () {
-        System.out.println("Enter the radius of the circle: ");
-        int rad = scanner.nextInt();
-        double p = 3.14;
-        System.out.println("Area of the circle: " + (rad*rad*p));
-        System.out.println("Perimeter of the cicle: " + (rad*2*p));
+
+        System.out.println("Enter the first number: ");
+        int a1 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter the second number: ");
+        int a2 = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter the third number: ");
+        int a3 = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Smallest number: " + smallestNumber(a1, a2, a3));
+    }
+
+    public static int smallestNumber(int s1, int s2, int s3) {
+        int firstmin = Math.min(s1, s2);
+        return Math.min(firstmin, s3);
+    }
+
+    public static void task3 () {
+
     }
 }
