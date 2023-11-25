5f457d724538c159415b0fe36a3721706fae54b7
Artur Ivanov <iarturv19@gmail.com>
2023-11-25 14:37:08 +0200

    Add pt for edu01

--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -1,15 +1,48 @@
 package com.softserve.edu01.pt;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
         // Practical tasks
 
         // Task 1
+        var scanner = new Scanner(System.in);
+        System.out.println("Input text: ");
+        String firstInput = scanner.nextLine();
+        String secondInput = scanner.nextLine();
+        String thirdInput = scanner.nextLine();
+        System.out.println("Output text:");
+        System.out.println(thirdInput);
+        System.out.println(secondInput);
+        System.out.println(firstInput);
 
         // Task 2
+        System.out.print("Number 1: ");
+        double inputFirst = scanner.nextDouble();
+        System.out.print("Number 2: ");
+        double inputSecond = scanner.nextDouble();
+        System.out.print("Number 3: ");
+        double inputThird = scanner.nextDouble();
+        double average = (inputFirst + inputSecond + inputThird) / 3;
+        System.out.print(average);
 
-        // Task 3...
+        // Task 3*
+        int a;
+        int b;
+        System.out.print("Enter a: ");
+        a = scanner.nextInt();
+        System.out.print("Enter b: ");
+        b = scanner.nextInt();
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.print("a / b = " + ((double) a / (double) b));
 
+        // Task 4*
+        System.out.println("How are you?");
+        String answer = scanner.nextLine();
+        System.out.print("You are " + answer);
     }
 }
---
+++ src/com/softserve/edu03/hw/App.java
@@ -0,0 +1,8 @@
+package com.softserve.edu03.hw;
+
+public class App {
+    public class Student {
+        private String Name;
+        private int age;
+    }
+}
