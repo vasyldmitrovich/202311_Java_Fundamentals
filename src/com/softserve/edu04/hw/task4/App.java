05d65bab5249137fee2ae250dfe77e48ee56d387
Olena Ponomarenko <ponomarenkoalena555@gmail.com>
2023-12-01 12:30:22 +0200

    Homework lesson 4 tasks 4

--- src/com/softserve/edu04/hw/task4/App.java
+++ src/com/softserve/edu04/hw/task4/App.java
@@ -1,7 +1,16 @@
 package com.softserve.edu04.hw.task4;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter count of students: ");
+        int count = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter season (e.g. winter, spring, e.c.t): ");
+        String season = scanner.nextLine();
+        Faculty lit = new Faculty(count, season);
+        System.out.println(lit);
     }
 }
--- src/com/softserve/edu04/hw/task4/Faculty.java
+++ src/com/softserve/edu04/hw/task4/Faculty.java
@@ -1,11 +1,54 @@
 package com.softserve.edu04.hw.task4;
 
 enum Season{
-    WINTER, SPRING, SUMMER, AUTUMN
+    WINTER("Winter exams"), SPRING("Second semester"), SUMMER("Vacation period"), AUTUMN("First semester");
+    final String engName;
+
+    Season(String engName) {
+        this.engName = engName;
+    }
+
+    @Override
+    public String toString() {
+        return "Season{" +
+                "engName='" + engName + '\'' +
+                '}';
+    }
 }
 
 public class Faculty {
     int numberOfStudents;
     Season season;
 
+    public Faculty() {
+    }
+
+    public Faculty(int numberOfStudents, String season) {
+        this.numberOfStudents = numberOfStudents;
+        this.season = Season.valueOf(season.toUpperCase());
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public Season getSeason() {
+        return season;
+    }
+
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public void setSeason(String season) {
+        this.season = Season.valueOf(season.toUpperCase());
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "numberOfStudents=" + numberOfStudents +
+                ", season=" + season +
+                '}';
+    }
 }
