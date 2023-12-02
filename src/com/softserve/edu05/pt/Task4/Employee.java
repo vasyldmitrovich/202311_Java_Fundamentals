7db74be08c95825abbbdc4bc759d314d4b5ab724
maxbaxter4 <maxkazanly@gmail.com>
2023-12-02 15:25:38 +0200

    Practical task 4, lesson 5

---
+++ src/com/softserve/edu05/pt/Task4/Employee.java
@@ -0,0 +1,82 @@
+package com.softserve.edu05.pt.Task4;
+
+import java.util.Scanner;
+
+public class Employee {
+    private final String name;
+    private final int departmentNumber;
+    private final double salary;
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public Employee(String name, int departmentNumber, double salary) {
+        this.name = name;
+        this.departmentNumber = departmentNumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee " +
+                "name : " + name +
+                ", department number: " + departmentNumber +
+                ", salary: " + salary;
+    }
+
+    public static void certainDepartment(Employee[] employees){
+
+        // All employees of a certain department (input department number in the console);
+        System.out.println("Enter department number");
+        int input = scanner.nextInt();
+        boolean notFound = true;
+
+        for (Employee employee: employees) {
+            if(employee.getDepartmentNumber() == input){
+                System.out.println(employee.toString());
+                notFound = false;
+            }
+        }
+        if(notFound){
+            System.out.printf("No employees found with number %d\n",input);
+        }
+    }
+
+    public static void sortInDescendingOrder(Employee[] employees){
+
+        //Arrange workers by the field salary in descending order.
+
+        System.out.println("Workers by salary in descending order: ");
+
+        //Bubble sort
+        for (int i = 0; i < employees.length - 1; i++) {
+            for (int j = 0; j < employees.length - i - 1; j++) {
+                if (employees[j].getSalary() < employees[j + 1].getSalary()) {
+                    // Swap arr[j] and arr[j+1]
+                    Employee temp = employees[j];
+                    employees[j] = employees[j + 1];
+                    employees[j + 1] = temp;
+                }
+            }
+        }
+
+        //output
+        for (Employee employee : employees) {
+            System.out.println(employee.toString());
+        }
+
+    }
+
+}
+
---
+++ src/com/softserve/edu05/pt/Task4/Main.java
@@ -0,0 +1,20 @@
+package com.softserve.edu05.pt.Task4;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Joe",1385,3000);
+        Employee employee2 = new Employee("Mike",1251,2900);
+        Employee employee3 = new Employee("Bob",1385,3900);
+        Employee employee4 = new Employee("Max",2001,5000);
+        Employee employee5 = new Employee("Alex",1251,1800);
+
+        Employee[] employees = new Employee[]{
+                employee1, employee2, employee3, employee4, employee5
+        };
+
+        Employee.certainDepartment(employees);
+        Employee.sortInDescendingOrder(employees);
+
+
+    }
+}
