09a912166d0a9808c4e85c6911c45a0bf5f140ef
Artur Ivanov <iarturv19@gmail.com>
2023-12-03 19:15:15 +0200

    save

---
+++ src/com/softserve/edu03/hw/task3/Main.java
@@ -0,0 +1,11 @@
+package com.softserve.edu03.hw.task3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Person person1 = new Person("Artur", "Ivanov");
+
+    }
+}
---
+++ src/com/softserve/edu03/hw/task3/Person.java
@@ -0,0 +1,47 @@
+package com.softserve.edu03.hw.task3;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getAge() {
+        LocalDate currentDate = LocalDate.now();
+        int currentYear = currentDate.getYear();
+        return currentYear - birthYear;
+    }
+}
