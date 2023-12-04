b9236342c6d8c7dbbb60b8b32d3617190c9ae086
Artur Ivanov <iarturv19@gmail.com>
2023-12-04 23:29:28 +0200

    Add task 3 for hw03

--- src/com/softserve/edu03/hw/task3/Main.java
+++ src/com/softserve/edu03/hw/task3/Main.java
@@ -1,11 +1,17 @@
 package com.softserve.edu03.hw.task3;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Person person1 = new Person("Artur", "Ivanov");
+        Person person1 = new Person("Artur", "Ivanov", 2001);
+        Person person2 = new Person("Taras", "Shevchenko", 1814);
+        Person person3 = new Person("Oleksandr", "Usyk", 1987);
+        Person person4 = new Person("Lys", "Mykyta", 1931);
+        Person person5 = new Person("Marta", "Skif", 1994);
 
+        System.out.println(person1.toString());
+        person1.changeName("Maksym", "Velykii");
+        System.out.println(person1.getFirstName());
+        System.out.println(person1.getLastName());
+        System.out.println(person2.Age());
     }
 }
--- src/com/softserve/edu03/hw/task3/Person.java
+++ src/com/softserve/edu03/hw/task3/Person.java
@@ -1,6 +1,7 @@
 package com.softserve.edu03.hw.task3;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Person {
     private String firstName;
@@ -13,6 +14,12 @@ public class Person {
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName, int birthYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
     }
 
     public String getFirstName() {
@@ -39,9 +46,44 @@ public class Person {
         this.birthYear = birthYear;
     }
 
-    public int getAge() {
+    public int Age() {
         LocalDate currentDate = LocalDate.now();
         int currentYear = currentDate.getYear();
         return currentYear - birthYear;
+    }
+
+    public void inputInfo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your first name: ");
+        this.firstName = scanner.nextLine();
+        System.out.println("Enter your last name: ");
+        this.lastName = scanner.nextLine();
+        System.out.println("Enter your birth year: ");
+        this.birthYear = scanner.nextInt();
+    }
+
+    public void outputInfo() {
+        System.out.println("First name: " + firstName);
+        System.out.println("Last name: " + lastName);
+        System.out.println("Birth year: " + birthYear);
+    }
+
+    public void changeName(String fn, String ln) {
+        if (fn != null && !fn.isEmpty()) {
+            this.firstName = fn;
+        }
+
+        if (ln != null && !ln.isEmpty()) {
+            this.lastName = ln;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName ='" + firstName + '\'' +
+                ", lastName ='" + lastName + '\'' +
+                ", birthYear = " + birthYear +
+                ", age = " + Age();
     }
 }
