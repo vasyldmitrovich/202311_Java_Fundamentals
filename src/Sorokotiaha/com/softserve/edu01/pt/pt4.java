ad05e1b3b355f19dfb8b71f52708776e75e988f0
Oleh <DwemOV@gmail.com>
2023-12-06 19:38:53 +0200

    pt edu1

---
+++ src/Sorokotiaha/com/softserve/edu01/pt/pt4.java
@@ -0,0 +1,12 @@
+package Sorokotiaha.com.softserve.edu01.pt;
+
+import java.util.Scanner;
+
+public class pt4 {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+        System.out.print("How are you?");
+        String answer = sc.nextLine();
+        System.out.println("You are " + answer) ;
+    }
+}
