ba23eedb0dfa1eb7d20c0716d998f5dc805ccab9
Olena Ponomarenko <ponomarenkoalena555@gmail.com>
2023-12-06 19:41:50 +0200

    Lesson 5 homework tasks 1-5

--- test/com/softserve/edu05/hw/task1/AppPart2Test.java
+++ test/com/softserve/edu05/hw/task1/AppPart2Test.java
@@ -3,13 +3,15 @@ package com.softserve.edu05.hw.task1;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class AppPart2Test {
 
     @Test
     @DisplayName("Work with 5 first positive numbers")
     void test1() {
         int[] arr = {1, 2, 4, 5, 6, 7, 8,};
-        AppPart2.getSumOrProduct(true, arr);
+        assertTrue(18 == AppPart2.getSumOrProduct(true, arr));
     }
 
     @Test
