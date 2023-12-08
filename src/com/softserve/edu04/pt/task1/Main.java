530140e1f26442ac38176ef48fb317fc21e1bc70
arturivanov <iarturv19@gmail.com>
2023-12-08 18:08:19 +0200

    add pt 1 for edu04

---
+++ src/com/softserve/edu04/pt/task1/Main.java
@@ -0,0 +1,29 @@
+package com.softserve.edu04.pt.task1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+
+        int count = 0;
+
+        if (num1 % 2 != 0) {
+            count++;
+        }
+        if (num2 % 2 != 0) {
+            count++;
+        }
+        if (num3 % 2 != 0) {
+            count++;
+        }
+
+        System.out.println("The number of odd numbers is " + count);
+    }
+}
