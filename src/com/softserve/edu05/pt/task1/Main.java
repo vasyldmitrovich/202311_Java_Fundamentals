51e21cd1d7a6204e483a8198c0b9518a39e6003e
arturivanov <iarturv19@gmail.com>
2023-12-10 20:55:26 +0200

    add pt 1 for edu05

---
+++ src/com/softserve/edu05/pt/task1/Main.java
@@ -0,0 +1,34 @@
+package com.softserve.edu05.pt.task1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] countries = {"Ukraine", "France", "USA", "Germany", "Austria"};
+        System.out.println(Arrays.toString(countries));
+        Arrays.sort(countries);
+        System.out.println(Arrays.toString(countries));
+
+        double[] numbers = {3, 8, 11, 6, 24};
+        double sum = 0.0;
+        for (int i = 0; i < numbers.length; i ++) {
+            sum += numbers[i];
+        }
+        System.out.println("The average value is " + sum / numbers.length);
+
+        System.out.print("Enter a word to check: ");
+        Scanner scanner = new Scanner(System.in);
+        String wordCheck = scanner.nextLine();
+        boolean found = false;
+        for (String v : countries) {
+            if (v.equals(wordCheck)) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            System.out.println("The word " + wordCheck + " is included in the array");
+        } else System.out.println("The word " + wordCheck + " is not included in the array");
+    }
+}
