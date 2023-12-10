473b4d4e66141ce85197c0806ecbe394c95e1140
Kornilenko Oleksandr <sanya.kornilenko14@gmail.com>
2023-12-10 21:41:17 +0200

    edu05 try to write a test(todo later)

---
+++ test/com/softserve/edu05/hw/HomeTask1/Task1Test.java
@@ -0,0 +1,16 @@
+package com.softserve.edu05.hw.HomeTask1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class Task1Test {
+
+    @Test
+    @DisplayName("Correct days in month")
+    void test1() {
+        var oldIn = System.in;
+
+    }
+
+
+}
