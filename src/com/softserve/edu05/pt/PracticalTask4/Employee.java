966ed75078b8f82e056820667d0b6ba2b99ff90f
Kornilenko Oleksandr <sanya.kornilenko14@gmail.com>
2023-12-10 16:23:53 +0200

    edu05 add solution for pt4

---
+++ src/com/softserve/edu05/pt/PracticalTask4/Employee.java
@@ -0,0 +1,34 @@
+package com.softserve.edu05.pt.PracticalTask4;
+
+public class Employee {
+    String name;
+    int departmentNumber;
+    double salary;
+
+    public Employee(String name, int departmentNumber, double salary) {
+        this.name = name;
+        this.departmentNumber = departmentNumber;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", departmentNumber=" + departmentNumber +
+                ", salary=" + salary +
+                '}';
+    }
+}
