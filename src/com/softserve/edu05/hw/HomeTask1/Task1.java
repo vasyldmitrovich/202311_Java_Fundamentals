9ffcb9204fea0df31af2dc111ae83778b158aad0
Kornilenko Oleksandr <sanya.kornilenko14@gmail.com>
2023-12-14 14:45:59 +0200

    edu 05 HomeTask1 Task1 Test added

--- src/com/softserve/edu05/hw/HomeTask1/Task1.java
+++ src/com/softserve/edu05/hw/HomeTask1/Task1.java
@@ -5,9 +5,30 @@ import java.util.Scanner;
 /*A month number and calculates the number of days in that month based on an array
 that contains the number of days in each month.*/
 public class Task1 {
-    private static final Scanner SCANNER = new Scanner(System.in);
+    //private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
+        int monthNumber = getMonthNumber();
+        try {
+            System.out.println(getDaysInMonth(monthNumber) + " days in " + getMonth(monthNumber));
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid month number: " + monthNumber);
+        }
+
+    }
+
+    static int getMonthNumber() {
+        var scanner = new Scanner(System.in);
+        System.out.print("Enter month number [1 - 12]: ");
+        int monthNumber = Integer.parseInt(scanner.nextLine());
+        if (monthNumber < 1 || monthNumber > 12) {
+            return -1; //ArrayIndexOutOfBoundsException
+        }
+        return monthNumber;
+    }
+
+    static String getMonth(int monthNumber) {
         String[] month = {
                 "January", "February",
                 "March", "April", "May",
@@ -15,13 +36,13 @@ public class Task1 {
                 "September", "October", "November",
                 "December"
         };
-        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        System.out.print("Enter month number [1 - 12]: ");
-        int monthNumber = Integer.parseInt(SCANNER.nextLine());
-        if (monthNumber < 1 || monthNumber > 12) {
-            throw new IllegalArgumentException("Invalid month number: " + monthNumber);
-        }
-        System.out.println(daysInMonth[monthNumber - 1] + " days in " + month[monthNumber - 1]);
-
+        return month[monthNumber - 1];
     }
+
+    static int getDaysInMonth(int monthNumber) {
+        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return daysInMonth[monthNumber - 1];
+    }
+
+
 }
--- test/com/softserve/edu05/hw/HomeTask1/Task1Test.java
+++ test/com/softserve/edu05/hw/HomeTask1/Task1Test.java
@@ -1,16 +1,62 @@
 package com.softserve.edu05.hw.HomeTask1;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class Task1Test {
-
-    @Test
-    @DisplayName("Correct days in month")
-    void test1() {
-        var oldIn = System.in;
-
+    private InputStream oldIn;
+    @BeforeEach
+    void saveSystemIn(){
+        oldIn = System.in;
+    }
+    @AfterEach
+    void RestoreSystemIn(){
+        System.setIn(oldIn);
     }
 
 
+    @Test
+    @DisplayName("Correct month returning by month number")
+    void getMonthTest() {
+        String expectedMoth = "June";
+        String res = Task1.getMonth(6);
+        assertEquals(expectedMoth, res);
+    }
+
+    @Test
+    @DisplayName("Correct days in month returning by month number")
+    void getDaysInMonthTest() {
+        int expectedDays = 30;
+        int res = Task1.getDaysInMonth(6);
+        assertEquals(expectedDays, res);
+    }
+
+    @Test
+    @DisplayName("Correct month number entered (input 1 - 12)")
+    void getMonthNumberTest1() {
+        provideInput("6");
+        int expectedNumber = 6;
+        var res = Task1.getMonthNumber();
+        assertEquals(expectedNumber, res);
+    }
+
+    @Test
+    @DisplayName("Correct month number entered (input NOT 1 - 12)")
+    void getMonthNumberTest2() {
+        provideInput("13");
+        int expectedNumber = -1;
+        var res = Task1.getMonthNumber();
+        assertEquals(expectedNumber, res);
+    }
+
+    private void provideInput(String text){
+        System.setIn(new ByteArrayInputStream(text.getBytes()));
+    }
 }
