12abc6910cf2a44e6cf95d2cfcb4ade22149d0b9
maxbaxter4 <maxkazanly@gmail.com>
2023-12-18 15:08:04 +0200

    7. Homework 1

--- src/com/softserve/edu07/hw/Task1/App.java
+++ src/com/softserve/edu07/hw/Task1/App.java
@@ -1,44 +1,27 @@
 package com.softserve.edu07.hw.Task1;
 
 import java.util.Arrays;
-import java.util.Comparator;
-
-import static java.util.Arrays.sort;
 
 public class App {
     public static void main(String[] args) {
 
         Employee[] employees = new Employee[]{
-                new SalariedEmployee("Ben",2000,"UHSd77"),
-                new ContractEmployee("Kate",23,33,"iaush77"),
-                new SalariedEmployee("Zack",3444,"UKJCS8"),
-                new ContractEmployee("Sanya",12,100,"LKASU3"),
-                new SalariedEmployee("Sergo",8223,"Udlkamc"),
-                new ContractEmployee("Leha",211,123,"(*#dkds"),
-                new SalariedEmployee("Misha",23,"I989ux"),
-                new ContractEmployee("Danu",532,33,"HIxias8")
+                new SalariedEmployee("Ben", 2000, "UHSd77"),
+                new ContractEmployee("Kate", 23, 33, "iaush77"),
+                new SalariedEmployee("Zack", 3444, "UKJCS8"),
+                new ContractEmployee("Sanya", 12, 100, "LKASU3"),
+                new SalariedEmployee("Sergo", 8223, "Udlkamc"),
+                new ContractEmployee("Leha", 211, 123, "(*#dkds"),
+                new SalariedEmployee("Misha", 23, "I989ux"),
+                new ContractEmployee("Danu", 532, 33, "HIxias8")
 
         };
 
 
-        Arrays.sort(employees,(e1,e2)->Double.compare(e2.getAverageSalary(), e1.getAverageSalary()));
+        Arrays.sort(employees, (e1, e2) -> Double.compare(e2.getAverageSalary(), e1.getAverageSalary()));
 
-        for (Employee employee : employees){
+        for (Employee employee : employees) {
             System.out.println(employee.toString());
         }
-
-
     }
-
-//    public static double getAverageSalary (Employee[] employees){
-//        double total = 0;
-//        double count = 0;
-//
-//        for (Employee employee : employees){
-//            total += employee.calculatePay();
-//            count++;
-//
-//        }
-//        return total / count;
-//    }
 }
--- src/com/softserve/edu07/hw/Task1/Employee.java
+++ src/com/softserve/edu07/hw/Task1/Employee.java
@@ -1,16 +1,12 @@
 package com.softserve.edu07.hw.Task1;
 
-import java.util.Comparator;
-
 public abstract class Employee implements Payment{
     private static int nextEmployeeId = 1;
 
     protected String employeeId;
 
-
-
     public Employee() {
-        this.employeeId = "E " + nextEmployeeId++;
+        this.employeeId = "E " + nextEmployeeId++;//authomatic id for each employee
     }
 
     @Override
