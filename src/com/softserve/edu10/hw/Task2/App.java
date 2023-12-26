ca9040deffa85014a3dfe9987e3c535a66078f17
maxbaxter4 <maxkazanly@gmail.com>
2023-12-26 18:37:37 +0200

    10. Homework 2

---
+++ src/com/softserve/edu10/hw/Task2/App.java
@@ -0,0 +1,57 @@
+package com.softserve.edu10.hw.Task2;
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+
+        Map <String,String> personMap = new LinkedHashMap<>();
+
+        personMap.put("Luniov","Artem");
+        personMap.put("Lebedev","Martin");
+        personMap.put("Shevchenko","Alexandra");
+        personMap.put("Levaya","Marina");
+        personMap.put("Marunevich","Alexandra");
+        personMap.put("Borov","Sergei");
+        personMap.put("Pitica","Petrea");
+        personMap.put("Ivanov","Orest");
+        personMap.put("Slavov","Ignat");
+        personMap.put("Kozimova","Riana");
+        delimiter();
+        print(personMap);
+        checkDuplicates(personMap);
+        delimiter();
+        System.out.println("At least two persons with the same first name: " + checkDuplicates(personMap));
+        delimiter();
+        removeSpecifiedName(personMap,"Orest");
+        System.out.println("After removing specify name: ");
+        print(personMap);
+
+
+
+    }
+    public static boolean checkDuplicates(Map<String,String> map){
+        Map<String, Integer> firstNameCountMap = new HashMap<>();
+
+        for (String firstName : map.values()) {
+            firstNameCountMap.put(firstName, firstNameCountMap.getOrDefault(firstName, 0) + 1);
+        }
+
+        return firstNameCountMap.values().stream().anyMatch(count -> count >= 2);
+    }
+
+    public static void removeSpecifiedName(Map<String,String> map, String specifiedName){
+        map.entrySet().removeIf(entry->entry.getValue().equals(specifiedName));
+    }
+
+    public static void delimiter(){
+        System.out.println("============");
+    }
+
+
+    public static void print(Map<String,String> map){
+        for (var person : map.entrySet()){
+            System.out.println(person.getKey() + " " + person.getValue());
+        }
+    }
+}
