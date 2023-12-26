cd53fbd639b030200a03fc749a86f2df9ea88428
maxbaxter4 <maxkazanly@gmail.com>
2023-12-26 23:52:50 +0200

    11. Practical task 1

---
+++ src/com/softserve/edu11/pt/Task1/App.java
@@ -0,0 +1,13 @@
+package com.softserve.edu11.pt.Task1;
+
+public class App {
+    public static void main(String[] args) {
+        String str1 = "SoftServe Academy";
+        String str2 = "SoftServe";
+        String str3 = "Academy";//for additional checking
+
+        System.out.println(str1.contains(str2));//Task 1 (main check). Ok
+        System.out.println(str2.contains(str3));
+        System.out.println(str1.contains(str3));
+    }
+}
