2df820c4f2246db3e81e91a5b41eadd199860656
maxbaxter4 <maxkazanly@gmail.com>
2023-12-30 17:52:26 +0100

    12. Practical task 1

---
+++ src/com/softserve/edu12/pt/Task1/App.java
@@ -0,0 +1,38 @@
+package com.softserve.edu12.pt.Task1;
+
+import java.util.Scanner;
+
+public class App {
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        while (true) {
+            try {
+                System.out.println("Enter side a");
+                a = Integer.parseInt(SCANNER.nextLine());
+                System.out.println("Enter side b");
+                b = Integer.parseInt(SCANNER.nextLine());
+               int area =  rectangleArea(a,b);
+                System.out.println("Area = " + area);
+                break;
+            } catch (NumberFormatException e){
+                System.out.println("Enter integer number");
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+
+
+    public static int rectangleArea (int a, int b){
+
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Sides should be non negative. a = " + a + " b = " + b + ".");
+        }
+        return a * b;
+
+    }
+}
