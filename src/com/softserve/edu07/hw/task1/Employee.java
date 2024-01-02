625f23fbf5599edd24efbb4cb19521eb200638ca
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2024-01-02 14:33:50 +0200

    I added some comment to code. Please make attention and fix bug if needed

--- src/com/softserve/edu07/hw/task1/Employee.java
+++ src/com/softserve/edu07/hw/task1/Employee.java
@@ -1,7 +1,7 @@
 package com.softserve.edu07.hw.task1;
 
 public abstract class Employee implements Payment{
-    String employeeId;
+    String employeeId;//Make fields private and add getters and setters
     String name;
 
     public Employee(String employeeId, String name) {
@@ -12,5 +12,5 @@ public abstract class Employee implements Payment{
     @Override
     public double calculatePay() {
         return 0;
-    }
+    }//If you do not do implementation you do must not implement this method in abstract class
 }
