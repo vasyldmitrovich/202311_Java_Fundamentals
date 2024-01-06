e7cb4f343b411ec37a229dc51fbafbdd964e5212
Kornilenko Oleksandr <sanya.kornilenko14@gmail.com>
2024-01-07 01:33:44 +0200

    edu 14 ht2 solution

--- src/com/softserve/edu14/hw/homeTask2/Emploee.java
+++ src/com/softserve/edu14/hw/homeTask2/Emploee.java
@@ -1,13 +1,32 @@
 package com.softserve.edu14.hw.homeTask2;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Emploee {
+    private String name;
 
+    public Emploee(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     static Optional<String> mostPopularName(Stream<Emploee> employees) {
-        return null;
+        return employees
+                .collect(Collectors.groupingBy((Emploee::getName), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey);
     }
 
 }
--- src/com/softserve/edu14/hw/homeTask2/Main.java
+++ src/com/softserve/edu14/hw/homeTask2/Main.java
@@ -1,4 +1,22 @@
 package com.softserve.edu14.hw.homeTask2;
 
+import java.util.stream.Stream;
+
 public class Main {
+    public static void main(String[] args) {
+        Stream<Emploee> emploeeList1 = Stream.empty();
+        System.out.println(Emploee.mostPopularName(emploeeList1));
+
+        Stream<Emploee> emploeeList2 = Stream.of(
+                new Emploee("Bob"),
+                new Emploee("Din"),
+                new Emploee("Sam"),
+                new Emploee("Sam")
+        );
+        System.out.println(Emploee.mostPopularName(emploeeList2));
+
+
+
+
+    }
 }
