84109a66c596c3dfd3c62bf47e45a9f1e14a69ab
Olena Ponomarenko <ponomarenkoalena555@gmail.com>
2024-01-08 15:11:26 +0200

    Lesson 14 pt

--- src/com/softserve/edu14/pt/App.java
+++ src/com/softserve/edu14/pt/App.java
@@ -2,7 +2,6 @@ package com.softserve.edu14.pt;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class App {
@@ -16,7 +15,7 @@ public class App {
         List<String> rezultList = list.stream().filter(t -> !t.isEmpty()).toList();
         System.out.println(rezultList);
         System.out.println("====================================================");
-        String stringUpperCase = rezultList.stream().map(t -> t.toUpperCase()).collect(Collectors.joining(","));
+        String stringUpperCase = rezultList.stream().map(String::toUpperCase).collect(Collectors.joining(","));
         System.out.println(stringUpperCase);
 
         System.out.println("=========================================================================");
@@ -24,7 +23,7 @@ public class App {
 
         //task2
         List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
-        System.out.println(primes.stream().count());
+        System.out.println((long) primes.size());
         System.out.println(primes.stream().min(Integer::compareTo).get());
         System.out.println(primes.stream().max(Integer::compareTo).get());
         System.out.println(primes.stream().mapToInt((t) -> t).summaryStatistics().getSum());
