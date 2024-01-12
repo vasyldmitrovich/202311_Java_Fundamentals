27ef1edc332f6185d9a272b13ec4b9a133896ba3
maxbaxter4 <maxkazanly@gmail.com>
2024-01-12 12:48:59 +0200

    15. Practical task 2

--- src/com/softserve/edu15/pt/Task2/Run1.java
+++ src/com/softserve/edu15/pt/Task2/Run1.java
@@ -1,7 +1,7 @@
 package com.softserve.edu15.pt.Task2;
 
 public class Run1 extends Thread{
-    @Override
+
     public void run() {
         for (int i = 0; i < 5; i++) {
             System.out.println("Hello, world");
--- src/com/softserve/edu15/pt/Task2/Run2.java
+++ src/com/softserve/edu15/pt/Task2/Run2.java
@@ -1,7 +1,7 @@
 package com.softserve.edu15.pt.Task2;
 
 public class Run2 extends Thread{
-    @Override
+
     public void run() {
         for (int i = 0; i < 5; i++) {
             System.out.println("Peace in the peace");
