a1d7a8641cf32177d9d7e65defd341e9fe909eed
maxbaxter4 <maxkazanly@gmail.com>
2024-01-24 19:05:08 +0200

    16. Practical task 2

--- src/com/softserve/edu16/pt/Task2/App.java
+++ src/com/softserve/edu16/pt/Task2/App.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class App {
+
     public static void main(String[] args) throws IOException {
 
         var fileName = "mytext.txt";
