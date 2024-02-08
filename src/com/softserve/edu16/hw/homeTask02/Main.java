b978adc5cb2055ed19489b79c024805499cc176a
Kornilenko Oleksandr <sanya.kornilenko14@gmail.com>
2024-02-08 15:06:41 +0200

    edu 16 hw2 solution

--- src/com/softserve/edu16/hw/homeTask02/Main.java
+++ src/com/softserve/edu16/hw/homeTask02/Main.java
@@ -1,4 +1,22 @@
 package com.softserve.edu16.hw.homeTask02;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
+    public static void main(String[] args) {
+        Path sourceFilePath = Paths.get("sourceFile.java");
+        Path destinationFilePath = Paths.get("destinationFile.java");
+
+        try {
+            String content = new String(Files.readAllBytes(sourceFilePath));
+            String modifiedContent = content.replaceAll("\\bpublic\\b", "private");
+
+            Files.write(destinationFilePath, modifiedContent.getBytes());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
